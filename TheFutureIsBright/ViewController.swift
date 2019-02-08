218818f35ba06aa59c4afb86a2d9996184ff19a7
Randall Brown <rbrown1@grubhub.com>
2019-02-08 07:14:22 -0500

    Initial Commit

---
+++ TheFutureIsBright/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheFutureIsBright
+//
+//  Created by RBrown on 2/8/19.
+//  Copyright © 2019 RBrown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
